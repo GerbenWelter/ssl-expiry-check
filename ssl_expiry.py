b9acdb1f827903cdecdb0d7ac408c929032c4800
Gerben Welter <gerben.welter@achmea.nl>
2021-03-09 20:40:05 +0100

    Some changes for running in a kubernetes cluster
    - Add support for a port number
    - Log to stdout instead of logger facility

--- ssl_expiry.py
+++ ssl_expiry.py
@@ -1,19 +1,16 @@
+#!/usr/bin/env python3
 # Author: Lucas Roelser <roesler.lucas@gmail.com>
+# URL: https://github.com/LucasRoesler/ssl-expiry-check
 # Modified from serverlesscode.com/post/ssl-expiration-alerts-with-lambda/
 
 import datetime
 import fileinput
-import logging
 import os
 import socket
 import ssl
 import time
 
-
-logger = logging.getLogger('SSLVerify')
-
-
-def ssl_expiry_datetime(hostname: str) -> datetime.datetime:
+def ssl_expiry_datetime(hostname: str, port: int) -> datetime.datetime:
     ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
 
     context = ssl.create_default_context()
@@ -24,8 +21,8 @@ def ssl_expiry_datetime(hostname: str) -> datetime.datetime:
     # 3 second timeout because Lambda has runtime limitations
     conn.settimeout(3.0)
 
-    logger.debug('Connect to {}'.format(hostname))
-    conn.connect((hostname, 443))
+    print(f'Connect to {hostname}:{port}')
+    conn.connect((hostname, port))
     ssl_info = conn.getpeercert()
     # parse the string from the certificate into a Python datetime object
     return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
@@ -34,45 +31,42 @@ def ssl_expiry_datetime(hostname: str) -> datetime.datetime:
 def ssl_valid_time_remaining(hostname: str) -> datetime.timedelta:
     """Get the number of days left in a cert's lifetime."""
     expires = ssl_expiry_datetime(hostname)
-    logger.debug(
-        'SSL cert for {} expires at {}'.format(
-            hostname, expires.isoformat()
-        )
-    )
+    print(f'SSL cert for {hostname}:{port} expires at {expires.isoformat()}')
     return expires - datetime.datetime.utcnow()
 
 
-def test_host(hostname: str, buffer_days: int=30) -> str:
+def test_host(hostname: str, port: int, buffer_days: int=30) -> str:
     """Return test message for hostname cert expiration."""
     try:
-        will_expire_in = ssl_valid_time_remaining(hostname)
+        will_expire_in = ssl_valid_time_remaining(hostname, port)
     except ssl.CertificateError as e:
-        return f'{hostname} cert error {e}'
+        return f'{hostname}:{port} cert error {e}'
     except ssl.SSLError as e:
-        return f'{hostname} cert error {e}'
+        return f'{hostname}:{port} cert error {e}'
     except socket.timeout as e:
-        return f'{hostname} could not connect'
+        return f'{hostname}:{port} could not connect'
     else:
         if will_expire_in < datetime.timedelta(days=0):
-            return f'{hostname} cert will expired'
+            return f'{hostname}:{port} cert will expired'
         elif will_expire_in < datetime.timedelta(days=buffer_days):
-            return f'{hostname} cert will expire in {will_expire_in}'
+            return f'{hostname}:{port} cert will expire in {will_expire_in}'
         else:
-            return f'{hostname} cert is fine'
+            return f'{hostname}:{port} cert is fine'
 
 
 if __name__ == '__main__':
-    loglevel = os.environ.get('LOGLEVEL', 'INFO')
-    numeric_level = getattr(logging, loglevel.upper(), None)
-    if not isinstance(numeric_level, int):
-        raise ValueError('Invalid log level: %s' % loglevel)
-    logging.basicConfig(level=numeric_level)
-
     start = time.time()
     for host in fileinput.input():
         host = host.strip()
-        logger.debug('Testing host {}'.format(host))
-        message = test_host(host)
-        print(message)
+        if ':' in host:
+            domain = host.split(':')[0]
+            port = host.split(':')[1]
+        else:
+            domain = host
+            port = 443
+        print(f'Testing host {domain}:{port}')
+        message = test_host(domain, port)
 
-    logger.debug('Time: {}'.format(time.time() - start))
+    while True:
+        time.sleep(1)
+
